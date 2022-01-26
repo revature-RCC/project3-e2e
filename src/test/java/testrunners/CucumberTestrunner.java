2aae19dc0446e9d89cb22d3a815020c26bdeb5b0
joesi8302 <joesi@cpp.edu>
2022-01-26 10:47:13 -0800

    added pagination and out of stock tests

--- src/test/java/testrunners/CucumberTestrunner.java
+++ src/test/java/testrunners/CucumberTestrunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "features/main.feature",
         glue = "steps",
-        dryRun = false
+        dryRun = true
 )
 public class CucumberTestrunner {
 }
